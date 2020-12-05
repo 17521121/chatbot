8a4ced53ffff646839428dc23b46d1089b679f29
Đặng Quốc Tiến <17521121@gm.uit.edu.vn>
2020-12-05 11:40:32 +0700

    finish

--- index.py
+++ index.py
@@ -1,17 +1,20 @@
 from flask import Flask, render_template, request
+from flask_fontawesome import FontAwesome
+
 app = Flask(__name__)
+fa = FontAwesome(app)
 
 @app.route('/')
 def index():
-   yourarg = request.args.get('user')
    return render_template('index.html')
 
-@app.route('/result', methods = ['POST', 'GET'])
-def result():
-   yourarg = flask.request.args.get('')
+@app.route('/chat', methods = ['POST'])
+def chat():
    if request.method == 'POST':
-      result = request.form
-      return render_template("result.html", result = result)
+      n1 = request.form["n1"]
+      n1 = request.form["n2"]
+      chat_text = request.form["chat_text"]
+      return chat_text
 
 if __name__ == '__main__':
    app.run(debug = True)
