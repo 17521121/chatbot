0c63fc77fcc3526be4a6bbaf1d7ed79029e0ccd7
Đặng Quốc Tiến <17521121@gm.uit.edu.vn>
2020-12-05 10:40:08 +0700

    xong giao diện

--- index.py
+++ index.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def student():
+def index():
+   yourarg = request.args.get('user')
    return render_template('index.html')
 
 @app.route('/result', methods = ['POST', 'GET'])
 def result():
+   yourarg = flask.request.args.get('')
    if request.method == 'POST':
       result = request.form
       return render_template("result.html", result = result)
